7b136732ccac73637d7a586f95e93812b382dff3
Sergey <xxsmotur@gmail.com>
2020-01-07 18:36:52 +0500

    Exporting of bot class

--- src/logic/Bot.ts
+++ src/logic/Bot.ts
@@ -1,9 +1,15 @@
 import { Telegram } from 'telegraf';
 
 
-class Bot {
+export class Bot {
     private bot: Telegram;
     constructor(tolen:string){
         this.bot = new Telegram(process.env.TELEGRAM_TOKEN, {});
     }
+
+    async init(){
+
+    }
 }
+
+export default Bot;
