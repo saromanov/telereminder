3f9d66f7264df445cd2d911f6210f68188aabc76
Sergey <xxsmotur@gmail.com>
2020-04-11 00:42:54 +0500

    Fixing on connection to mongo

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
-import mongoose from 'mongoose';
+import * as mongoose from 'mongoose';
+import Bot from './logic/Bot';
 
-mongoose.connect(`mongodb://localhost:27017/${process.env.DATABASE_HOST}`, {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect("mongodb://localhost:27017/${process.env.DATABASE_HOST}", (err:any)=> {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log('connected to mongo');
+  }
 });
-mongoose.connection.on('error', err => {
+mongoose.connection.on('error', () => {
   process.exit(1);
 });
+
+mongoose.connection.on('open', () => {
+    const bot = new Bot(process.env.TELEGRAM_TOKEN);
+    bot.init();
+})
 
